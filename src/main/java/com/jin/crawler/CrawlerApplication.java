5dbdcda7ef81e76f22efcd52cf913dac186b60ad
zyang0419 <pzzxh001@163.com>
2019-11-27 11:24:15 +0800

    修改主启动类

--- src/main/java/com/jin/crawler/CrawlerApplication.java
+++ src/main/java/com/jin/crawler/CrawlerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+//20191127添加注释
 @SpringBootApplication(scanBasePackages = "com.jin.crawler")
 @EnableTransactionManagement(proxyTargetClass = true)
 public class CrawlerApplication extends SpringBootServletInitializer {
